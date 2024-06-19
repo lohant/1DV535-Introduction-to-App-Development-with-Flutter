15c9c7fee50def1bd351d86c02306d6ecb72b0fa
Anton Holst <anton.holst@hotmail.com>
2024-06-19 16:32:24 +0200

    Removed debug-banner

--- assignment1_the_me_card/assignment1_the_me_card/lib/main.dart
+++ assignment1_the_me_card/assignment1_the_me_card/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'First Flutter',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Personal Card'),
